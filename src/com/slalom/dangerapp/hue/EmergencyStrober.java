2147c983455ede0dd616479c28b52c5963671c8d
Jeremy Duvall <matchless.tone@gmail.com>
2014-01-05 14:57:43 -0500

    Assets and lighting changes

--- src/com/slalom/dangerapp/hue/EmergencyStrober.java
+++ src/com/slalom/dangerapp/hue/EmergencyStrober.java
@@ -1,5 +1,6 @@
 package com.slalom.dangerapp.hue;
 
+import android.util.Log;
 import com.philips.lighting.hue.sdk.PHHueSDK;
 import com.philips.lighting.model.PHBridge;
 import com.philips.lighting.model.PHLight;
@@ -7,13 +8,13 @@ import com.philips.lighting.model.PHLightState;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class EmergencyStrober implements Runnable {
     private static final int MAX_HUE=65535;
-    private static final int BLINKY_INTERVAL=1000;
+    private static final int BLINKY_INTERVAL=1500;
     private static final int WAITING_FOR_BRIDGE = 2000;
-    private boolean running = false;
+    private static final int RED = 0;
+    private volatile boolean running = false;
     private PHHueSDK sdk;
 
     public EmergencyStrober(PHHueSDK sdk) {
@@ -33,15 +34,12 @@ public class EmergencyStrober implements Runnable {
                         new ArrayList<PHLight>() :
                         bridge.getResourceCache().getAllLights();
                 if(bridge != null && lights.size() > 0) {
+                    LightNode head = this.convertToCircularLinkedList(lights);
+                    this.initEmergencyState(bridge, head);
+                    Thread.sleep(BLINKY_INTERVAL);
                     while(running){
                         // strobe lights
-                        Random rand = new Random();
-                        for(PHLight light : lights) {
-                            PHLightState state = new PHLightState();
-                            state.setHue(rand.nextInt(MAX_HUE));
-                            bridge.updateLightState(light, state);
-                        }
-
+                        updateEmergencyStates(bridge, head);
                         Thread.sleep(BLINKY_INTERVAL);
                     }
                 } else {
@@ -49,7 +47,119 @@ public class EmergencyStrober implements Runnable {
                 }
             }
         } catch (InterruptedException exc) {
-            this.terminate();
+            if(!this.running) {
+                this.terminate();
+                Log.w("JDUV", "Terminated due to thread exception");
+            }
+        }
+    }
+
+    private void initEmergencyState(PHBridge bridge, LightNode head) {
+        LightNode current = head;
+        do {
+            PHLightState state = new PHLightState();
+            state.setHue(RED);
+
+            if(current == head)
+            {
+                state.setOn(false);
+            }
+
+            bridge.updateLightState(current.getLight(), state);
+            current = current.getNext();
+
+        } while(current != head);
+    }
+
+    private void updateEmergencyStates(PHBridge bridge, LightNode head) {
+        LightNode current = head;
+        LightNode previous = null;
+
+        do {
+            previous = current == null ? null : current.getPrevious();
+
+            if(previous != null && !previous.getLight().getLastKnownLightState().isOn()) {
+                PHLightState currentState = new PHLightState();
+                PHLightState previousState = new PHLightState();
+                previousState.setOn(true);
+                currentState.setOn(false);
+                current.setState(currentState);
+                previous.setState(previousState);
+            }
+
+            current = current.getNext();
+
+        } while(current != head);
+
+        this.commitStateChanges(bridge, head);
+        Log.w("JDUV", "Done updating state");
+    }
+
+    private void commitStateChanges(PHBridge bridge, LightNode head) {
+        LightNode current = head;
+        do {
+            if(current.getState() != null) {
+                bridge.updateLightState(current.getLight(), current.getState());
+                current.setState(null);
+            }
+            current = current.getNext();
+        } while (current != head);
+        Log.w("JDUV", "Committed state changes");
+    }
+
+    private LightNode convertToCircularLinkedList(List<PHLight> lights) {
+        LightNode head = new LightNode(lights.get(0));
+        LightNode secondLight = new LightNode(lights.get(2));
+        LightNode thirdLight = new LightNode(lights.get(1));
+
+        head.setNext(secondLight);
+        head.setPrevious(thirdLight);
+
+        secondLight.setNext(thirdLight);
+        secondLight.setPrevious(head);
+
+        thirdLight.setNext(head);
+        thirdLight.setPrevious(secondLight);
+
+        return head;
+    }
+
+    private class LightNode {
+        private LightNode next;
+        private LightNode previous;
+        private PHLight light;
+        private PHLightState state;
+
+        public LightNode(PHLight light) {
+            this.light = light;
+        }
+
+        public LightNode getNext() {
+            return this.next;
+        }
+
+        public void setNext(LightNode next) {
+            this.next = next;
+        }
+
+        public LightNode getPrevious() {
+            return this.previous;
+        }
+
+        public void setPrevious(LightNode previous) {
+            this.previous = previous;
+        }
+
+        public PHLight getLight() {
+            return this.light;
+        }
+
+        public PHLightState getState() {
+            return this.state;
+        }
+
+        public void setState(PHLightState toSet) {
+            this.state = toSet;
         }
     }
 }
--- src/com/slalom/dangerapp/views/HomeView.java
+++ src/com/slalom/dangerapp/views/HomeView.java
@@ -5,22 +5,37 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import com.philips.lighting.hue.sdk.PHHueSDK;
+import com.philips.lighting.model.PHBridge;
+import com.philips.lighting.model.PHLight;
+import com.philips.lighting.model.PHLightState;
 import com.slalom.dangerapp.R;
 import com.slalom.dangerapp.hue.EmergencyStrober;
 import com.slalom.dangerapp.hue.SdkInitializer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeView extends Activity {
     private boolean blinky = false;
     private SdkInitializer initializer;
+    private static final int BLUE = 46920;
+    private static final int GREEN = 25500;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.home);
 
         this.initializer = new SdkInitializer(getApplicationContext());
-        PHHueSDK sdk = initializer.getConnectedSdk();
+        final PHHueSDK sdk = initializer.getConnectedSdk();
         final EmergencyStrober strober = new EmergencyStrober(sdk);
-        final Thread stroberThread = new Thread(strober);
+        final ThreadWrapper wrapper = new ThreadWrapper();
+
+        final Button initLights = (Button) findViewById(R.id.init_lights);
+        initLights.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                clearLights(sdk, BLUE);
+            }
+        });
 
         final Button blinkyButton = (Button) findViewById(R.id.blinkyon);
         blinkyButton.setOnClickListener(new View.OnClickListener() {
@@ -28,7 +43,9 @@ public class HomeView extends Activity {
                 // Perform action on click
                 if(!blinky) {
                     blinky = true;
-                    stroberThread.start();
+                    Thread t = new Thread(strober);
+                    wrapper.setWrapped(t);
+                    t.start();
                 }
             }
         });
@@ -37,10 +54,11 @@ public class HomeView extends Activity {
         noBlinkyButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 try {
-                    if(blinky) {
+                    if(blinky && wrapper.getWrapped() != null) {
                         blinky = false;
                         strober.terminate();
-                        stroberThread.join();
+                        wrapper.getWrapped().join();
+                        clearLights(sdk, GREEN);
                     }
                 } catch (InterruptedException ie) {
                     // Nothing to do really.
@@ -53,5 +71,35 @@ public class HomeView extends Activity {
     protected void onDestroy() {
         this.initializer.disconnectSdk();
         super.onDestroy();
+    }
+
+    private void clearLights(PHHueSDK sdk, int color) {
+        PHBridge bridge = sdk.getSelectedBridge();
+        List<PHLight> lights = bridge == null ?
+                new ArrayList<PHLight>() :
+                bridge.getResourceCache().getAllLights();
+        if(bridge != null && lights.size() > 0) {
+            // strobe lights
+            for(PHLight light : lights) {
+                PHLightState state = new PHLightState();
+                state.setOn(true);
+                state.setHue(color);
+                state.setBrightness(200);
+                state.setSaturation(254);
+                bridge.updateLightState(light, state);
+            }
+        }
+    }
+
+    private class ThreadWrapper {
+        private Thread wrapped;
+
+        public Thread getWrapped() {
+            return this.wrapped;
+        }
+
+        public void setWrapped(Thread toWrap) {
+            this.wrapped = toWrap;
+        }
     }
 }
